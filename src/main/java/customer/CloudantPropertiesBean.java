d01c113a080647e37d6be12e8e467f5c15f56fb4
Ivan Mlakic <mlakic.ivan@gmail.com>
2019-01-09 20:50:24 +0000

    Error in import

--- src/main/java/customer/CloudantPropertiesBean.java
+++ src/main/java/customer/CloudantPropertiesBean.java
@@ -1,7 +1,6 @@
 package customer;
 
-import
-org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
